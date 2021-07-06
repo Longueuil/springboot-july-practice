b70cc90b16d9ee455b82763a6a981018e9717d59
Longueuil <vladimirtocarica@gmail.com>
2021-07-06 12:07:33 -0400

    added custom injecting

--- src/main/java/com/example/julypractice/Controllers/MainPageController.java
+++ src/main/java/com/example/julypractice/Controllers/MainPageController.java
@@ -1,10 +1,22 @@
 package com.example.julypractice.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainPageController {
+
+    // inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/team")
+    public String getInfo(){
+        return "Coach: " + coachName + " Team: " + teamName;
+    }
 
     @GetMapping("/")
     public String helloApp(){
